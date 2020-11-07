d96616aa7bb3e8bd9cdddd826103b87e2d33f7a3
elzodxon <sharofaddinov01@mail.ru>
2020-11-07 11:52:05 +0500

    Finished dog years

---
+++ dogYears/dog.js
@@ -0,0 +1,13 @@
+const myName = prompt('Ismingiz kim? ')
+
+let myAge = Number(prompt("Yoshingiz nechida?"))
+
+let firstTwoYears = 2 * 10.5;
+
+let nextYears = myAge - 2;
+nextYears *=4; // nextYears = nextYears * 4
+
+let dogYears = firstTwoYears + nextYears;
+
+console.log(`Mening ismim ${myName}. Yoshim ${myAge} da. It yilida hisoblaganda, yoshim ${dogYears} `);
+
